637999814dfa55bd9f3c09851022965efb8b907a
iftekharx <iftekharx86@gmail.com>
2023-05-08 10:04:32 +0600

    added MuiSelect

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,12 @@ import './App.css'
 import MuiTypography from './components/MuiTypography'
 import MuiButton from './components/MuiButton'
 import MuiTextField from './components/MuiTextField'
+import MuiSelect from './components/MuiSelect'
 
 function App() {
   return (
     <div className="App">
-      <MuiTextField />
+      <MuiSelect />
     </div>
   )
 }
